e26fae7ca98eeb9651cde1215427c5e2e59a5966
Gatmaitan <arnoldgatmaitan28@gmail.com>
2019-09-05 19:55:48 +0800

    Chap 5 added maven support

--- Chapter_05/Spittr/src/main/java/spittr/config/SpitterWebInitializer.java
+++ Chapter_05/Spittr/src/main/java/spittr/config/SpitterWebInitializer.java
@@ -4,13 +4,27 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 import spittr.web.WebConfig;
 
+/**
+ * Under the covers, AbstractAnnotationConfigDispatcherServletInitializer
+ * creates both a DispatcherServlet and a ContextLoaderListener
+ */
 public class SpitterWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-  
+
+  /**
+   * @Configuration class’s returned getRootConfigClasses() will be used to configure the application context created by
+   * ContextLoaderListener
+   * @return
+   */
   @Override
   protected Class<?>[] getRootConfigClasses() {
     return new Class<?>[] { RootConfig.class };
   }
 
+  /**
+   * @Configuration classes returned from getServletConfigClasses() will define beans for
+   * DispatcherServlet’s application context.
+   * @return
+   */
   @Override
   protected Class<?>[] getServletConfigClasses() {
     return new Class<?>[] { WebConfig.class };
